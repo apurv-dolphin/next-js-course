d16859ff2bb52055d1c6750b62e035536cf6ff6b
apurv-dolphin <apurv.dolphinwebsolution@gmail.com>
2022-11-21 18:06:27 +0530

    genrate a order pdf

--- pages/order.js
+++ pages/order.js
@@ -1,19 +1,38 @@
-import { Box } from "@material-ui/core";
+import { Box, Button } from "@material-ui/core";
 import { Alert } from "@material-ui/lab";
-import React from "react";
+import React, { createRef } from "react";
 import styles from "../styles/checkout.module.css";
 import OrderContain from "./components/ordercontain";
+import Pdf from "react-to-pdf";
+import Link from "next/link";
 
-export default function Order({ cart, total  }) {
+export default function Order({ cart, total, clearCart }) {
   const date = new Date();
   const time = date.getTime();
+  const ref = createRef();
   return (
     <Box className={styles.maincontainer}>
       <div className={styles.header}>
         <h2> OrderId : {time} </h2>
         <Alert severity="success">Your order has been succesfully place.</Alert>
       </div>
-      <OrderContain cart={cart} total={total} />
+      <div ref={ref} style={{ width: "100%" }}>
+        <OrderContain cart={cart} total={total} />
+      </div>
+      <Box>
+        <Link href={"/product"}>
+        <Button onClick={clearCart}>Back</Button>
+        </Link>
+        <Pdf
+          targetRef={ref}
+          filename={`"orderId${time}.pdf"`}
+          x={0.5}
+          y={0.5}
+          scale={0.6}
+        >
+          {({ toPdf }) => <Button onClick={toPdf}>Genrate order PDF</Button>}
+        </Pdf>
+      </Box>
     </Box>
   );
 }
